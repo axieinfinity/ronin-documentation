0556ee8d5a98a28dc225cf4779e0d2913266a4e8
Phát <tienphat1501@gmail.com>
2022-03-02 12:34:25 +0700

    feat: adding eslint-config, typescript support

--- README.md
+++ README.md
@@ -3,14 +3,18 @@
 Ronin's Document is built with [docusaurus](https://docusaurus.io/).
 
 ## Running locally
+
 1. Install [Node.js](https://nodejs.org/en/download/) version >= 14.0
 2. Install [Yarn](https://classic.yarnpkg.com/en/docs/install) classic
 3. Install dependencies
-```
+
+```bash
 yarn install
 ```
+
 4. Start local server
-```
+
+```bash
 yarn start
 ```
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import clsx from 'clsx'
+import React from 'react'
+
+import Layout from '@theme/Layout'
+
+import HomepageFeatures from '../components/HomepageFeatures'
+import styles from './index.module.css'
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">Welcome to Ronin Network</h1>
-        <p className="hero__subtitle">Let's build together</p>
+        <p className="hero__subtitle">Let&apos;s build together</p>
       </div>
     </header>
-  );
+  )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Ronin Network`}
-      description="Ronin Documentation">
+    <Layout title={`Ronin Network`} description="Ronin Documentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
-  );
+  )
 }
