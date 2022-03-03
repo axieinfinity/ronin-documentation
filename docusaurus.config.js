b65dec4fd5077ba5c9c597a4f48a6fd5bd822b6c
Phát <tienphat1501@gmail.com>
2022-03-03 17:58:15 +0700

    feat: remove blog on footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,10 +97,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/axieinfinity/',
               },
