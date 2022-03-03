6122959cda1055a0482fff2910cf4dc0fa260e4b
Phát Mai <tienphat1501@gmail.com>
2022-03-03 16:56:14 +0700

    feat: update welcome page (#4)
    * feat: update welcome page
    
    * fix: update css val
    
    * fix: fix CI
    
    * fix: adding card box shadow
    
    * feat: update dark mode ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Layout from '@theme/Layout'
+
+import { Features } from '../components/homepage-features/Features'
+import styles from './index.module.scss'
+
+const Home: React.FC = () => {
+  return (
+    <Layout title={`Ronin Network`} description="Ronin Documentation">
+      <div className={styles.background}>
+        <header className={styles.heroBanner}>
+          <h1 className={styles.heroBannerHeading}>Welcome to Ronin Network</h1>
+          <p className={styles.heroBannerSubHeading}>Let&apos;s build together</p>
+        </header>
+
+        <main className={styles.main}>
+          <div>
+            <img src="/img/mascot.svg" className={styles.mascot} />
+          </div>
+
+          <Features />
+        </main>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
