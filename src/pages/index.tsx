03d015c05463affa46a2ea28272663a6733d97a2
yumemily <emilyyum@gmail.com>
2022-11-23 16:58:32 +0700

    feat: use docusaurus announcement banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,43 +4,25 @@ import Layout from '@theme/Layout'
 
 import { Features } from '../components/homepage-features/Features'
 import styles from './index.module.scss'
-import ExternalLink from './external-link.svg'
 
 const Home: React.FC = () => {
   return (
-    <>
-      <div className={styles.alertBanner}>
-        <span className={styles.pin}>🎉</span>
-        <strong>Saigon</strong> - an open testnet for Ronin, is live!
-        <a
-          className={styles.link}
-          href='https://saigon-docs.roninchain.com/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Checkout Saigon documentation
-          <ExternalLink className={styles.link}/>
-        </a>
+    <Layout title={`Ronin Network`} description='Ronin Documentation'>
+      <div className={styles.background}>
+        <header className={styles.heroBanner}>
+          <h1 className={styles.heroBannerHeading}>Welcome to Ronin Network</h1>
+          <p className={styles.heroBannerSubHeading}>
+            Let&apos;s build together
+          </p>
+        </header>
+        <main className={styles.main}>
+          <div>
+            <img src='/img/mascot.svg' className={styles.mascot} />
+          </div>
+          <Features />
+        </main>
       </div>
-      <Layout title={`Ronin Network`} description='Ronin Documentation'>
-        <div className={styles.background}>
-          <header className={styles.heroBanner}>
-            <h1 className={styles.heroBannerHeading}>
-              Welcome to Ronin Network
-            </h1>
-            <p className={styles.heroBannerSubHeading}>
-              Let&apos;s build together
-            </p>
-          </header>
-          <main className={styles.main}>
-            <div>
-              <img src='/img/mascot.svg' className={styles.mascot} />
-            </div>
-            <Features />
-          </main>
-        </div>
-      </Layout>
-    </>
+    </Layout>
   )
 }
 
