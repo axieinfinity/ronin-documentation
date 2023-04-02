ed2303e3166f98fe18c6749bc757270c92aba27c
yumemily <emilyyum@gmail.com>
2023-04-02 14:47:37 +0700

    fix: trigger mouseover event for searchbar

--- src/theme/SearchBar/index.tsx
+++ src/theme/SearchBar/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import SearchBar from '@theme-original/SearchBar'
 
 import styles from './index.module.scss'
 
 export default function SearchBarWrapper(props) {
+
+useEffect(() => {
+  const searchInput = document.getElementById('search_input_react');
+  if(searchInput) {
+    searchInput.dispatchEvent(new MouseEvent('mouseover', {
+      'view': window,
+      'bubbles': true,
+      'cancelable': true
+    }));
+  }
+},[])
+
   return (
     <div className={styles.searchWrapper}>
-      <SearchBar {...props} />
+      <SearchBar {...props} placeholder="meep" />
     </div>
   )
 }
