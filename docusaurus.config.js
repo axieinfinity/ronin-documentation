206bcf129e3fb7b6f85f9db8e68a1e45b2b88197
Sergey Rodin <sergeyrodin.sr@gmail.com>
2023-04-25 17:15:09 +0700

    chore: shorten link text in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -174,7 +174,7 @@ const config = {
                 href: 'https://validator.roninchain.com/',
               },
               {
-                label: 'Ronin developer documentation',
+                label: 'Developer documentation',
                 href: 'http://docs.skymavis.com/docs/getting-started',
               },
             ],
